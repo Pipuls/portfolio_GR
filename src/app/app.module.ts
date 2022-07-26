32fbf4a1c4735e2ce59a2edd14e08dcaba7d7bb7
Pipuls <56545116+Pipuls@users.noreply.github.com>
2022-07-26 12:15:27 -0300

    login y ruteo de app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,10 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HysSkilsComponent } from './components/hys-skils/hys-skils.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { EducationComponent } from './components/education/education.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 
 @NgModule({
@@ -25,11 +30,15 @@ import { EducationComponent } from './components/education/education.component';
     FooterComponent,
     HysSkilsComponent,
     EducationComponent,
+    ProjectsComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
 
   ],
   providers: [],
